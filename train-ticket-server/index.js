2810a8c21cea31a47735e555a96992c183ae7901
lirenkang <572657177@qq.com>
2021-07-12 20:15:04 +0800

    Update index.js

--- train-ticket-server/index.js
+++ train-ticket-server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const apiMocker = require('mocker-api');
-
+// 1
 const app = express();
 
 apiMocker(app, path.resolve('./mocker/mocker.js'));
